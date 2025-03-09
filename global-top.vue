28c8af6fb9034d650c02935c03b5f78bfd0aac2a
johnlin <john830110@gmail.com>
2025-03-10 00:56:18 +0800

    feat: 新增頁碼

---
+++ global-top.vue
@@ -0,0 +1,9 @@
+<!-- hide the footer from "cover" layout -->
+<template>
+    <Scroll />
+    <footer class="absolute bottom-5 left-0 right-7 text-right">
+        <SlideCurrentNo />
+        <!-- / -->
+        <!-- <SlidesTotal /> -->
+    </footer>
+</template>
